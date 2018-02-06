7d270522295e5031cb7db4d2f94e685d97ab4a08
Jeremy R <jeremy@devmounta.in>
2018-02-06 09:54:07 -0700

    first

---
+++ states.js
@@ -0,0 +1,9 @@
+let states = {
+    california:{population:38332521, size:162695},
+    texas:{population:26448193, size:268580},
+    newYork:{population:19651127, size:54556},
+    florida:{population:19552860, size:65754},
+    illinois:{population:12882135, size:57914},
+    pennsylvania:{population:12773801, size:46055},
+    ohio:{population:11570808, size:44824}
+  }
